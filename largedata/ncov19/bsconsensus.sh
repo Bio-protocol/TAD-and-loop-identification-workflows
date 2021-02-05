efe6ba091890388c199dedd9e8e9d03bd0aa4624
Jinliang Yang <yangjl0930@gmail.com>
2021-02-04 21:15:57 -0600

    modified the largedata

--- largedata/ncov19/bsconsensus.sh
+++
@@ -1,1 +0,0 @@
-/beegfs/group_dv/software/source/raxml-ng_v0.9.0/raxml-ng --consense MRE --tree RAxML_bootstrap.ncov9 --prefix consMRE
--- largedata/ncov19/plotbs_on_ml.sh
+++
@@ -1,1 +0,0 @@
-/beegfs/group_dv/software/source/raxml-ng_v0.9.0/raxml-ng --support --tree RAxML_bestTree.allcodons --bs-trees bak/RAxML_bootstrap.allcodons --bs-metric TBE

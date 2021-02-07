ea56aff04a516bb3ba9af55a5a889bb66237b18b
Jinliang Yang <yangjl0930@gmail.com>
2021-02-07 12:26:56 -0600

    minor updates

--- workflow/1_run_fastqc.sh
+++ workflow/1_run_fastqc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # normalize your dir to the root level of the repo
-normalDir="`cd "${../}";pwd`"
+normalDir="`cd "${..}";pwd`"
 echo "${normalDir}"
 
 fastqc -o output/ -f fastq -t 1 input/reads1.fastq input/reads1.fastq > output/run.log

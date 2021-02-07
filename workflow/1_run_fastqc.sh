3d064972d5c3bac86223947722c65044b1ccbe0a
Jinliang Yang <yangjl0930@gmail.com>
2021-02-07 12:22:39 -0600

    test the workflow

---
+++ workflow/1_run_fastqc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# normalize your dir to the root level of the repo
+normalDir="`cd "${../}";pwd`"
+echo "${normalDir}"
+
+fastqc -o output/ -f fastq -t 1 input/reads1.fastq input/reads1.fastq > output/run.log

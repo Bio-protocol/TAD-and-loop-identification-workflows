734249d04e88c01653454880f969ab5e15ed27f8
Jinliang Yang <yangjl0930@gmail.com>
2021-02-07 12:34:51 -0600

    updates

--- workflow/1_run_fastqc.sh
+++ workflow/1_run_fastqc.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
 # normalize your dir to the root level of the repo
-normalDir="`cd "${..}";pwd`"
-echo "${normalDir}"
+# Save the current working directory in an environment variable.
+INITIAL_WORKING_DIRECTORY=$(pwd)
+
+# This line changes to current working directory to where
+function canonicalPath
+{
+    local path="$1" ; shift
+    if [ -d "$path" ]
+    then
+        echo "$(cd "$path" ; pwd)"
+    else
+        local b=$(basename "$path")
+        local p=$(dirname "$path")
+        echo "$(cd "$p" ; pwd)/$b"
+    fi
+}
+
+# usage example
+mycanonicalpath=$(canonicalPath "$../")
 
 fastqc -o output/ -f fastq -t 1 input/reads1.fastq input/reads1.fastq > output/run.log
